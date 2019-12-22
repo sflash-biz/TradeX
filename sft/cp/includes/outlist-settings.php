6f7cef958a27649d8f7b692d1882b1c3b0eba86c
sflash-biz <sflash@sflash.biz>
2019-12-22 20:46:24 +0300

    Outlist Settings - additional interface for additional spots

--- sft/cp/includes/outlist-settings.php
+++ sft/cp/includes/outlist-settings.php
@@ -22,12 +22,13 @@ global $g_counter, $g_color;
                 <tbody>
                 <?php
 
-                $OUTLIST_WEIGHTS_FIELDS = 15;
+                $OUTLIST_WEIGHTS_FIELDS_MIN = 15;
 
                 $main_outlist = load_outlist(FILE_OUTLIST_MAIN);
                 $g_counter = 1;
                 $g_color = '#ececec';
                 $outlist_weights = explode('|', file_get_contents(FILE_OUTLIST_WEIGHTS));
+                $OUTLIST_WEIGHTS_FIELDS = count($outlist_weights) > $OUTLIST_WEIGHTS_FIELDS_MIN ? count($outlist_weights) : $OUTLIST_WEIGHTS_FIELDS_MIN;
                 $weights_sum = array_sum($outlist_weights);
 
                 $weights_stamp = array();
@@ -59,12 +60,14 @@ global $g_counter, $g_color;
         </div>
 
         <div id="dialog-help">
-            <a href="http://sflash.biz/sft-dist-pub/docs/outlist-settings.html" target="_blank"><img src="images/help-22x22.png"></a>
+            <a href="docs/outlist-settings.html" target="_blank"><img src="images/help-22x22.png"></a>
         </div>
 
         <div id="dialog-buttons">
             <img src="images/activity-16x16.gif" height="16" width="16" border="0" title="Working...">
-            <input type="submit" id="button-save" value="Save">
+            <input type="button" id="button-spot-plus" value=" + ">
+            <input type="button" id="button-spot-minus" value=" - ">
+            <input type="submit" id="button-save" value="Save" style="margin-left: 10px;">
             <input type="button" id="dialog-button-cancel" value="Cancel" style="margin-left: 10px;">
         </div>
 
@@ -77,172 +80,203 @@ global $g_counter, $g_color;
 
 function _outlist_table_header()
 {
-    ?>
-    <table class="item-table no-wrap fsize-9pt" border="0" cellspacing="0" cellpadding="4" align="center" style="width: 465px; min-width: 465px;">
-        <thead>
-        <tr>
-            <td style="width: 30px;"></td>
-            <td class="ta-center" style="width: 60px;">Weight</td>
-            <td class="ta-center" style="width: 160px;">% of Total</td>
-            <td class="ta-center">Site</td>
-        </tr>
-        </thead>
-    <?php
-}
-
-function _outlist_table_row_main($trade, $percent, $points)
-{
-    global $g_color, $g_counter;
-
-    $g_color = $g_color == '#ffffff' ? '#ececec' : '#ffffff';
-
-        ?>
-        <tr bgcolor="<?php echo $g_color; ?>">
-          <td class="ta-right" style="padding-right: 4px;">
-            <?php echo $g_counter; ?>
-          </td>
-          <td class="ta-right" style="padding-right: 4px;">
-             <span><input type="text" size="5" value="<?php echo $points; ?>" name="weight_<?php echo $g_counter; ?>" maxlength="5" pattern="[0-9]+"></span>
-          </td>
-          <td style="padding: 0px;" class="va-middle">
-            <div class="percent-bar ta-left" style="width: <?php echo $percent; ?>%;" id="percent-bar-<?php echo $g_counter; ?>">
-              <?php if( $percent >= 60 ) echo "<span id='percent-val-$g_counter'>$percent%</span>"; ?>
-            </div>
-            <span class="va-middle" id='percent-val-<?php echo $g_counter; ?>'><?php if( $percent < 60 ) echo "$percent%"; ?></span>
-          </td>
-          <td class="ta-right"><?php echo $trade[0]; ?></td>
-        </tr>
-<?php
-
-    $g_counter++;
-}
-
 ?>
-
-<script>
-
-    var fields_amount = 15;
-
-    var out_points_default_1 = [    // Normal
-        700,
-        500,
-        350,
-        270,
-        200,
-        150,
-        100,
-        80,
-        50,
-        30
-    ];
-
-    var out_points_default_2 = [    // Dynamic
-        1000,
-        600,
-        350,
-        270,
-        200,
-        150,
-        100,
-        80,
-        50,
-        30
-    ];
-
-    var out_points_default_3 = [    // TE
-        1000,
-        650,
-        423,
-        275,
-        179,
-        116,
-        75,
-        49,
-        32,
-        21,
-        14,
-        9
-    ];
-
-    var out_points_default_4 = [    // ATX v2
-        2920,
-        1945,
-        1300,
-        865,
-        575,
-        385,
-        255,
-        170,
-        115,
-        75,
-        50,
-        33,
-        23,
-        15,
-        10
-    ];
-
-
-    function format_float(original, after_dot) {
-        var div_mult = 1;
-        for (var i = 0; i < after_dot; i++) div_mult *= 10;
-        return Math.round(original*div_mult)/div_mult;
+<table id="outlist-settings-table" class="item-table no-wrap fsize-9pt" border="0" cellspacing="0" cellpadding="4" align="center" style="width: 465px; min-width: 465px;">
+    <thead>
+    <tr>
+        <td style="width: 30px;"></td>
+        <td class="ta-center" style="width: 60px;">Weight</td>
+        <td class="ta-center" style="width: 160px;">% of Total</td>
+        <td class="ta-center">Site</td>
+    </tr>
+    </thead>
+    <?php
     }
 
-    var func_change_wight_fields = function() {
-        var arr_fields_weights = [];
-        var val;
-        for (var i = 1; i <=fields_amount; i++)
-        {
-            val = parseInt( $('input[name="weight_' + i + '"]').val() );
-            arr_fields_weights[i-1] = isNaN(val) ? 0 : val;
+    function _outlist_table_row_main($trade, $percent, $points)
+    {
+        global $g_color, $g_counter;
+
+        $g_color = $g_color == '#ffffff' ? '#ececec' : '#ffffff';
+
+        ?>
+        <tr bgcolor="<?php echo $g_color; ?>" id="outlist-table-row-<?php echo $g_counter; ?>">
+            <td class="ta-right" style="padding-right: 4px;">
+                <?php echo $g_counter; ?>
+            </td>
+            <td class="ta-right" style="padding-right: 4px;">
+                <span><input type="text" size="5" value="<?php echo $points; ?>" name="weight_<?php echo $g_counter; ?>" maxlength="5" pattern="[0-9]+"></span>
+            </td>
+            <td style="padding: 0px;" class="va-middle">
+                <div class="percent-bar ta-left" style="width: <?php echo $percent; ?>%;" id="percent-bar-<?php echo $g_counter; ?>">
+                    <?php if( $percent >= 60 ) echo "<span id='percent-val-$g_counter'>$percent%</span>"; ?>
+                </div>
+                <span class="va-middle" id='percent-val-<?php echo $g_counter; ?>'><?php if( $percent < 60 ) echo "$percent%"; ?></span>
+            </td>
+            <td class="ta-right"><?php echo $trade[0]; ?></td>
+        </tr>
+        <?php
+
+        $g_counter++;
+    }
+
+    ?>
+
+    <script>
+
+        var fields_amount = <?=$OUTLIST_WEIGHTS_FIELDS;?>;
+        var fields_amount_min = <?=$OUTLIST_WEIGHTS_FIELDS_MIN;?>;
+
+        var out_points_default_1 = [    // Normal
+            700,
+            500,
+            350,
+            270,
+            200,
+            150,
+            100,
+            80,
+            50,
+            30
+        ];
+
+        var out_points_default_2 = [    // Dynamic
+            1000,
+            600,
+            350,
+            270,
+            200,
+            150,
+            100,
+            80,
+            50,
+            30
+        ];
+
+        var out_points_default_3 = [    // TE
+            1000,
+            650,
+            423,
+            275,
+            179,
+            116,
+            75,
+            49,
+            32,
+            21,
+            14,
+            9
+        ];
+
+        var out_points_default_4 = [    // ATX v2
+            2920,
+            1945,
+            1300,
+            865,
+            575,
+            385,
+            255,
+            170,
+            115,
+            75,
+            50,
+            33,
+            23,
+            15,
+            10
+        ];
+
+
+        function format_float(original, after_dot) {
+            var div_mult = 1;
+            for (var i = 0; i < after_dot; i++) div_mult *= 10;
+            return Math.round(original*div_mult)/div_mult;
         }
-        var out_points_sum = arr_fields_weights.reduce(function(a, b) { return a + b; }, 0);
-        for (i = 1; i <=fields_amount; i++)
-        {
-            $('input[name="weight_' + i + '"]').attr('value', arr_fields_weights[i-1] ? arr_fields_weights[i-1] : '');
-            var percent_val = arr_fields_weights[i-1] ? format_float(arr_fields_weights[i-1] * 100 / out_points_sum, 2) + '%' : '0.00%';
-            $('#percent-bar-' + i).css('width', percent_val);
-            $('#percent-val-' + i).text(percent_val);
+
+        var func_change_wight_fields = function() {
+            var arr_fields_weights = [];
+            var val;
+            for (var i = 1; i <=fields_amount; i++)
+            {
+                val = parseInt( $('input[name="weight_' + i + '"]').val() );
+                arr_fields_weights[i-1] = isNaN(val) ? 0 : val;
+            }
+            var out_points_sum = arr_fields_weights.reduce(function(a, b) { return a + b; }, 0);
+            for (i = 1; i <=fields_amount; i++)
+            {
+                $('input[name="weight_' + i + '"]').attr('value', arr_fields_weights[i-1] ? arr_fields_weights[i-1] : '');
+                var percent_val = arr_fields_weights[i-1] ? format_float(arr_fields_weights[i-1] * 100 / out_points_sum, 2) + '%' : '0.00%';
+                $('#percent-bar-' + i).css('width', percent_val);
+                $('#percent-val-' + i).text(percent_val);
+            }
+        };
+
+        $('input[name^="weight_"]').change(
+            func_change_wight_fields
+        );
+
+        $('#dialog-button-default-1').click(function() {
+            for (var i = 1; i <=fields_amount; i++)
+            {
+                $('input[name="weight_' + i + '"]').attr('value', out_points_default_1[i-1] ? out_points_default_1[i-1] : '');
+            }
+            func_change_wight_fields();
+        });
+
+        $('#dialog-button-default-2').click(function() {
+            for (var i = 1; i <=fields_amount; i++)
+            {
+                $('input[name="weight_' + i + '"]').attr('value', out_points_default_2[i-1] ? out_points_default_2[i-1] : '');
+            }
+            func_change_wight_fields();
+        });
+
+        $('#dialog-button-default-3').click(function() {
+            for (var i = 1; i <=fields_amount; i++)
+            {
+                $('input[name="weight_' + i + '"]').attr('value', out_points_default_3[i-1] ? out_points_default_3[i-1] : '');
+            }
+            func_change_wight_fields();
+        });
+
+        $('#dialog-button-default-4').click(function() {
+            for (var i = 1; i <=fields_amount; i++)
+            {
+                $('input[name="weight_' + i + '"]').attr('value', out_points_default_4[i-1] ? out_points_default_4[i-1] : '');
+            }
+            func_change_wight_fields();
+        });
+
+        function _outlist_table_row_main(g_counter) {
+            return '<tr bgcolor="#d9d9d9" id="outlist-table-row-' + g_counter + '">' +
+                '<td class="ta-right" style="padding-right: 4px;">' +
+                g_counter +
+                '</td>' +
+                '<td class="ta-right" style="padding-right: 4px;">' +
+                '<span><input type="text" size="5" value="" name="weight_' + g_counter + '" maxlength="5" pattern="[0-9]+"></span>' +
+                '</td>' +
+                '<td style="padding: 0;" class="va-middle">' +
+                '<div class="percent-bar ta-left" style="width: 0;" id="percent-bar-' + g_counter + '">' +
+                '<span id="percent-val-"' + g_counter + '>0.00%</span>' +
+                '</div>' +
+                '<span class="va-middle" id="percent-val-' + g_counter + '"></span>' +
+                '</td>' +
+                '<td class="ta-right"></td>' +
+                '</tr>';
         }
-    };
 
-    $('input[name^="weight_"]').change(
-        func_change_wight_fields
-    );
+        var outlist_settings_table_body = $("#outlist-settings-table tbody");
+        var c_line = fields_amount;
 
+        $('#button-spot-plus').click(function() {
+            c_line++;
+            outlist_settings_table_body.append(_outlist_table_row_main(c_line));
+        });
 
-
-    $('#dialog-button-default-1').click(function() {
-        for (var i = 1; i <=fields_amount; i++)
-        {
-            $('input[name="weight_' + i + '"]').attr('value', out_points_default_1[i-1] ? out_points_default_1[i-1] : '');
-        }
-        func_change_wight_fields();
-    });
-
-    $('#dialog-button-default-2').click(function() {
-        for (var i = 1; i <=fields_amount; i++)
-        {
-            $('input[name="weight_' + i + '"]').attr('value', out_points_default_2[i-1] ? out_points_default_2[i-1] : '');
-        }
-        func_change_wight_fields();
-    });
-
-    $('#dialog-button-default-3').click(function() {
-        for (var i = 1; i <=fields_amount; i++)
-        {
-            $('input[name="weight_' + i + '"]').attr('value', out_points_default_3[i-1] ? out_points_default_3[i-1] : '');
-        }
-        func_change_wight_fields();
-    });
-
-    $('#dialog-button-default-4').click(function() {
-        for (var i = 1; i <=fields_amount; i++)
-        {
-            $('input[name="weight_' + i + '"]').attr('value', out_points_default_4[i-1] ? out_points_default_4[i-1] : '');
-        }
-        func_change_wight_fields();
-    });
-
-</script>
+        $('#button-spot-minus').click(function() {
+            if (c_line > fields_amount_min) {
+                $('#outlist-settings-table tbody tr:last').remove();
+                c_line--;
+            }
+        });
+    </script>
