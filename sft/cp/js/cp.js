32507e07b1fac1a8735830500898f665589b2c19
sflash-biz <sflash@sflash.biz>
2019-12-22 04:10:16 +0300

    JS: extras not null for default error message

--- sft/cp/js/cp.js
+++ sft/cp/js/cp.js
@@ -335,6 +335,7 @@ function globalAjaxError(e, xhr, text, exception)
             if( exception )
             {
                 detail_message = 'Exception: ' + exception.message;
+                extras = '';
             }
             break;
     }
